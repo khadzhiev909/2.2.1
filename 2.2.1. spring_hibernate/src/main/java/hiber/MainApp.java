fae85028c1f24def27bf5c872cab804c3249e614
Ependi Khadzhiev <khadzhiev909@gmail.com>
2024-04-25 19:43:07 +0300

    задача 2.2.1 - сделанна

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,11 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Mersedes", 223)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("BMW", 4)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Mazda", 7)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Lada", 2002)));
+
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +28,11 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
-         System.out.println();
+         System.out.println("Car = "+user.getCar().toString());
       }
 
+      List<User> userList = userService.getUserByCarModelAndSeries("Mersedes", 223);
+      userList.stream().forEach(System.out::println);
       context.close();
    }
 }
